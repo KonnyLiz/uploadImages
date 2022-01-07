147108eeed10c10e11adb66811e9f64c0bf46227
Carlos Padilla <konnylizamaya@gmail.com>
2022-01-06 20:21:19 -0600

    mostrando las img cargadas

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Item } from 'src/app/models/item.interface';
 
 @Component({
   selector: 'app-photos',
@@ -7,8 +10,15 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class PhotosComponent implements OnInit {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore
+  ) {
+    this.itemsCollection = afs.collection<Item>('img');
+    this.items = this.itemsCollection.valueChanges();
+  }
 
   ngOnInit(): void {
   }
